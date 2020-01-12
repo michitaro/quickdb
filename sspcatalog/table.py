1a3ac5e6886492e4f8c0afcf4299eb07ed54fc8f
michitaro.koike <michitaro.nike@gmail.com>
2020-01-12 11:38:59 +0900

    can deal with pdr2

--- sspcatalog/table.py
+++ sspcatalog/table.py
@@ -39,13 +39,17 @@ class Table(object):
                 k = self._slice[k]
         return Table(self.worker, self._patch_dir, k, self.npy_loader)
 
-    @memoize
     def column(self, colref):
         if len(colref) == 1:
+            if colref[0] == 'object_id':
+                return self.slice(serialize.load(f'{self._patch_dir}/object_id.npy'))
             return self.table_info(colref[0])
         else:
             table_ref, column_ref = self._resolve_colref(colref)
-            return self._bin_table(table_ref).column(column_ref)
+            if column_ref.endswith('_mag'):
+                return nanojansky2abmag(self._bin_table(table_ref).column(f'{column_ref[:-4]}_flux'))
+            else:
+                return self._bin_table(table_ref).column(column_ref)
 
     def table_info(self, key):
         value = None
@@ -57,11 +61,15 @@ class Table(object):
             raise RuntimeError(f'no such table_info key: {key}')
         return numpy.full(len(self), value)
 
-    @memoize
     def null_check(self, colref, yes):
-        table_ref, column_ref = self._resolve_colref(colref)
+        table_ref, _ = self._resolve_colref(colref)
         is_null = self._bin_table(table_ref).is_null()
         return (is_null == yes and numpy.ones or numpy.zeros)(len(self), dtype=bool)
+
+    @cached_property
+    def rerun_meta(self):
+        with open(f'{self._patch_dir}/../../meta.pickle', 'rb') as f:
+            return pickle.load(f)
 
     @memoize
     def _resolve_colref(self, colref):
@@ -71,15 +79,24 @@ class Table(object):
     @memoize
     def __len__(self):
         if self._slice is None:
-            return self.meta['n_objects']
+            if self.pdr2:
+                return self.meta['size']
+            else:
+                return self.meta['n_objects']
         else:
             return len(self._slice)
+
+    # TODO: cleanup
+    @cached_property
+    def pdr2(self):
+        return 'size' in self.meta
 
     @cached_property
     def meta(self):
         with open(f'{self._patch_dir}/meta.pickle', 'rb') as f:
             return pickle.load(f)
 
+    @memoize
     def _bin_table(self, dirname):
         assert isinstance(dirname, tuple)
         assert all('.' not in p for p in dirname) and all('/' not in p for p in dirname)
@@ -104,7 +121,7 @@ class BinTable(object):
         with open(fname, 'rb') as f:
             return pickle.load(f)
 
-    @memoize
+    # TODO cleanup
     def column(self, name):
         if name in self.flags:
             return self.flag(name)
@@ -112,9 +129,35 @@ class BinTable(object):
         if os.path.exists(fname):
             return self.table.slice(self.table.npy_loader.load(fname))
         else:
-            return self.table.slice(self._dummy(name))
+            if self.table.pdr2:
+                return self._pdr2_dummy(name)
+            else:
+                return self.table.slice(self._dummy(name))
+    
+    @cached_property
+    def pdr2_meta(self):
+        ttype, fname = self._dirname.split('/')[-2:]
+        if ttype == 'forced':
+            if fname == 'universal':
+                return self.table.rerun_meta['forced_universal']
+            return self.table.rerun_meta['forced_filter']
+        if ttype == 'meas':
+            if fname == 'position':
+                return self.table.rerun_meta['meas_position']
+            return self.table.rerun_meta['meas_position']
+        raise RuntimeError(f'Unknown table table: {ttype}')
 
-    @memoize
+
+    def _pdr2_dummy(self, name):
+        if name in self.pdr2_meta['dtype']:
+            dtype, shape = self.pdr2_meta['dtype'][name]
+            shape = list(shape)
+            shape[0] = len(self.table)
+            return nans(shape, dtype)
+        else:
+            raise RuntimeError(f'Unknown column {name}')
+        
+    
     def _dummy(self, name):
         array_meta = self.meta.get('missing')
         if array_meta is not None:
@@ -123,19 +166,27 @@ class BinTable(object):
                 return nans(shape, dtype)
         raise RuntimeError(f'no such file: {self._dirname}/{name}.npy')
 
-    @memoize
     def flag(self, name):
         i, j = self.flags[name]
         flag = self.column(f'flags-{i}')
         return flag & (1 << j) != 0
 
-    @cached_property
+    @property
     def flags(self):
+        # TODO cleanup
+        if self.table.pdr2:
+            return self.pdr2_meta['flags']
         return self.meta['flag']
+
+    # TODO cleanup
+    @cached_property
+    def flags_meta(self):
+        with open(f'{self._dirname}/flags-meta.pickle', 'rb') as f:
+            return pickle.load(f)
 
     @memoize
     def is_null(self):
-        return not os.path.exists(f'{self._dirname}/id.npy')
+        return os.path.exists(self._dirname)
 
 
 def nans(shape, dtype):
@@ -151,24 +202,17 @@ def nans(shape, dtype):
     return a
 
 
+def nanojansky2abmag(flux):
+    # m_{\text{AB}}\approx -2.5\log _{10}\left({\frac {f_{\nu }}{\text{Jy}}}\right)+8.90
+    # Jy = 3631 jansky
+    return -2.5 * numpy.log10(flux * (10**-9) / 3631.)
+    # return -2.5 * numpy.log10(flux) - 31.4000656223
+    # return 2.5 * (32 - numpy.log10(flux)) - 48.6
+
+
 class NpyLoader(object):
-    @memoize
     def load(self, path):
-        import fcntl
-        with open('/dev/shm/quickdb', 'wb') as f:
-            try:
-                fcntl.flock(f, fcntl.LOCK_EX)
-                return serialize.load(path)
-            finally:
-                fcntl.flock(f, fcntl.LOCK_UN)
-    # def load(self, path):
-    #     import fcntl
-    #     with open('/dev/shm/quickdb', 'wb') as f:
-    #         try:
-    #             fcntl.flock(f, fcntl.LOCK_EX)
-    #             return serialize.load(path)
-    #         finally:
-    #             fcntl.flock(f, fcntl.LOCK_UN)
+        return serialize.load(path)
 
 
 if __name__ == '__main__':
