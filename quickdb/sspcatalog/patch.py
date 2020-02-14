8c914f58d6abe31e9c0817d0cdd83342b538ceb7
michitaro.koike <michitaro.nike@gmail.com>
2020-02-14 13:32:54 +0900

    optimize

--- quickdb/sspcatalog/patch.py
+++ quickdb/sspcatalog/patch.py
@@ -236,7 +236,8 @@ class NpyCache:
 def flux2mag(a: numpy.ndarray):
     # m_{\text{AB}}\approx -2.5\log _{10}\left({\frac {f_{\nu }}{\text{Jy}}}\right)+8.90
     # Jy = 3631 jansky
-    return -2.5 * numpy.log10(a * (10**-9) / 3631.)
+    # return -2.5 * numpy.log10(a * (10**-9) / 3631.)
+    return -1.0857362047581294 * numpy.log(a) + 31.40006562228223
 
 
 def coord_ra(XYZ):
